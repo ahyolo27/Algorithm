eb9e03d7914e1ca8761e0660de45728f3941a06a
ahyolo27 <102876175+ahyolo27@users.noreply.github.com>
2025-02-16 23:02:23 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 16856 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, arr[];
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        input(); // 입력
+        set(0);
+
+        System.out.println(sb);
+    }
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+    }
+
+    static void set(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++)
+                sb.append(arr[i]).append(" ");
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (depth == 0 || (depth > 0 && arr[depth - 1] <= i)) {
+                arr[depth] = i;
+                set(depth + 1);
+            }
+        }
+    }
+}
