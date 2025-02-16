034c53a4d7589c215c14b579e4f73552c689c21b
ahyolo27 <102876175+ahyolo27@users.noreply.github.com>
2025-02-16 23:13:44 +0900

    [Silver III] Title: N과 M (5), Time: 288 ms, Memory: 28500 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, list[], ans[];
+    static boolean isSelected[];
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        input(); // 입력
+        set(0);
+
+        System.out.println(sb);
+    }
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        list = new int[N];
+        ans = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            list[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(list);
+        isSelected = new boolean[list[list.length - 1] + 1];
+    }
+
+    static void set(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++)
+                sb.append(ans[i]).append(" ");
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!isSelected[list[i]]) {
+                isSelected[list[i]] = true;
+                ans[depth] = list[i];
+                set(depth + 1);
+                isSelected[list[i]] = false;
+            }
+        }
+    }
+}
