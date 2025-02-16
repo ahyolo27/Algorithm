0bf4a00227d4d94eb023e56c3252d758629a7290
ahyolo27 <102876175+ahyolo27@users.noreply.github.com>
2025-02-16 22:41:04 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 15852 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, arr[];
+
+    public static void main(String[] args) throws IOException {
+
+        input(); // 입력
+        set(1, 0);
+    }
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+    }
+
+    static void set(int start, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++)
+                System.out.print(arr[i] + " ");
+            System.out.println();
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            arr[depth] = i;
+            set(i + 1, depth + 1);
+        }
+    }
+}
