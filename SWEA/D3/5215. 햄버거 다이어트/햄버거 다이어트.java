38680a84ad81866748f46baa0c45ffb55404eede
ahyolo27 <102876175+ahyolo27@users.noreply.github.com>
2025-02-25 15:11:04 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,073 ms, Memory: 27,748 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -11,9 +11,7 @@ public class Solution {
         for (int tc = 1; tc <= TC; tc++) {
 
             input(); // 입력
-
-            for (int i = 1; i <= N; i++)
-                makeHamburger(0, 0, 0, i);
+            makeHamburger();
 
             System.out.println("#" + tc + " " + max);
         }
@@ -35,14 +33,47 @@ public class Solution {
         }
     }
 
-    static void makeHamburger(int idx, int sumT, int sumK, int r) {
-        if (r == 0) { // 다 뽑은 경우
-            if (sumK <= L)
-                max = Math.max(max, sumT);
-            return;
-        }
+    static void makeHamburger() {
+        int selection[] = new int[N];
 
-        for (int i = idx; i < N; i++)
-            makeHamburger(i + 1, sumT + T[i], sumK + K[i], r - 1);
+        for (int r = 1; r <= N; r++) {
+            Arrays.fill(selection, 0);
+            for (int i = N - r; i < N; i++)
+                selection[i] = 1;
+            do {
+                int sumT = 0;
+                int sumK = 0;
+                for (int i = 0; i < N; i++) {
+                    if (selection[i] == 1) {
+                        sumT += T[i];
+                        sumK += K[i];
+                    }
+                }
+                if (sumK <= L)
+                    max = Math.max(max, sumT);
+            } while (nextPermutation(selection));
+        }
+    }
+
+    static boolean nextPermutation(int arr[]) {
+        int i = arr.length - 1;
+        while (i > 0 && arr[i - 1] >= arr[i]) i--;
+        if (i == 0) return false;
+
+        int j = arr.length - 1;
+        while (arr[i - 1] >= arr[j]) j--;
+
+        swap(arr, i - 1, j);
+
+        int k = arr.length - 1;
+        while (i < k) swap(arr, i++, k--);
+
+        return true;
+    }
+
+    static void swap(int arr[], int a, int b) {
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
     }
 }
