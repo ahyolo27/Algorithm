82293bfc5ab26290c953f12cb0eba4ffbea98e28
ahyolo27 <102876175+ahyolo27@users.noreply.github.com>
2025-02-27 16:10:56 +0900

    [D4] Title: 정사각형 방, Time: 768 ms, Memory: 117,248 KB -BaekjoonHub

---
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -0,0 +1,80 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    static int T, N, map[][], max, minIdx;
+    static int dr[] = {-1, 0, 1, 0}, dc[] = {0, 1, 0, -1};
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static class Pos {
+        int r, c;
+
+        Pos(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            input(); // 입력
+
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < N; j++)
+                    move(i, j); // 이동
+
+            System.out.println("#" + t + " " + minIdx + " " + max);
+        }
+    }
+
+    static void input() throws IOException {
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        max = -1; // 초기화
+        minIdx = -1;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++)
+                map[i][j] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    static void move(int startR, int startC) {
+        boolean visited[][] = new boolean[N][N];
+        int cnt = 1;
+
+        Queue<Pos> q = new LinkedList<>();
+        q.offer(new Pos(startR, startC));
+        visited[startR][startC] = true;
+
+        while (!q.isEmpty()) {
+            Pos p = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nr = p.r + dr[i];
+                int nc = p.c + dc[i];
+                if (check(nr, nc) && !visited[nr][nc] && map[p.r][p.c] + 1 == map[nr][nc]) {
+                    visited[nr][nc] = true;
+                    cnt++;
+                    q.offer(new Pos(nr, nc));
+                }
+            }
+        }
+
+        if (max <= cnt) {
+            if (max == cnt)
+                minIdx = Math.min(minIdx, map[startR][startC]);
+            else
+                minIdx = map[startR][startC];
+            max = cnt;
+        }
+    }
+
+    static boolean check(int r, int c) {
+        return 0 <= r && r < N && 0 <= c && c < N;
+    }
+}
