619dc7d7605f15106c9913f75352c2a4dac215ff
ahyolo27 <102876175+ahyolo27@users.noreply.github.com>
2025-03-05 17:17:03 +0900

    [D2] Title: 나무 높이, Time: 97 ms, Memory: 26,368 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -2,31 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-    static int N, H, trees[], max, ans;
+    static int T, N, h[], MAX, days;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(br.readLine());
-        for (int t = 0; t < N; t++) {
+        StringBuilder sb = new StringBuilder();
 
+        T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
             input(); // 입력
             water(); // 나무에 물 주기
 
-            System.out.println("#" + (t + 1) + " " + ans);
+            sb.append("#").append(t).append(" ").append(days).append("\n");
         }
+        System.out.println(sb);
     }
 
     static void input() throws IOException {
         StringTokenizer st;
 
-        H = Integer.parseInt(br.readLine());
-        trees = new int[H];
-        max = 0;
+        N = Integer.parseInt(br.readLine());
+        h = new int[N];
+        days = 0;
+        MAX = 0;
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < H; i++) {
-            trees[i] = Integer.parseInt(st.nextToken());
-            max = Math.max(max, trees[i]); // 최대값 갱신
+        for (int i = 0; i < N; i++) {
+            h[i] = Integer.parseInt(st.nextToken());
+            MAX = Math.max(MAX, h[i]); // 최대값 갱신
         }
     }
 
@@ -34,24 +37,24 @@ public class Solution {
         int cnt1 = 0;
         int cnt2 = 0;
 
-        for (int i = 0; i < H; i++) {
-            int remain = max - trees[i];
+        for (int i = 0; i < N; i++) {
+            int remain = MAX - h[i];
             cnt1 += remain % 2;
             cnt2 += remain / 2;
         }
 
-        while (true) {
-            if (cnt2 - cnt1 > 1) {
-                cnt2 -= 1;
-                cnt1 += 2;
-            } else if (cnt1 - cnt2 > 1) {
-                cnt2 = cnt1 - 1; // 물 안 주는 날
-            } else break;
+        if (cnt1 > cnt2)
+            days = cnt1 * 2 - 1;
+        else if (cnt1 == cnt2)
+            days = cnt1 * 2;
+        else { // cnt1 < cnt2
+            days += cnt1 * 2;
+            cnt2 -= cnt1;
+            days += ((cnt2 * 2) / 3) * 2;
+            if ((cnt2 * 2) % 3 == 1)
+                days += 1;
+            else if ((cnt2 * 2) % 3 == 2)
+                days += 2;
         }
-
-        if (cnt2 > cnt1) {
-            ans = cnt1 + cnt2 + 1;
-        } else
-            ans = cnt1 + cnt2;
     }
 }
