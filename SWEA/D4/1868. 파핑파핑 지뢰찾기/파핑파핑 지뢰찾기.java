8a7e6cce54e3de5713fcb4ecd6d89b2b56493099
ahyolo27 <102876175+ahyolo27@users.noreply.github.com>
2025-03-06 12:16:29 +0900

    [D4] Title: 파핑파핑 지뢰찾기, Time: 385 ms, Memory: 105,624 KB -BaekjoonHub

--- SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
@@ -1,16 +1,11 @@
 import java.io.*;
 import java.util.*;
 
-import java.io.*;
-import java.util.*;
-
 public class Solution {
-    static int n, click;
-    static int map[][];
-    static boolean visited[][];
-    static Queue<Pos> q = new LinkedList<>();
-    static int dr[] = {0, -1, 0, 1, -1, -1, 1, 1};
-    static int dc[] = {1, 0, -1, 0, 1, -1, 1, -1};
+    static int T, N, count[][], cnt;
+    static String map[][];
+    static int dr[] = {-1, -1, 0, 1, 1, 1, 0, -1}, dc[] = {0, 1, 1, 1, 0, -1, -1, -1};
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     static class Pos {
         int r, c;
@@ -22,67 +17,69 @@ public class Solution {
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int t = Integer.parseInt(br.readLine());
-
-        for (int tc = 0; tc < t; tc++) {
-            n = Integer.parseInt(br.readLine());
-            map = new int[n][n];
-            visited = new boolean[n][n];
-
-            for (int i = 0; i < n; i++) {
-                String s = br.readLine();
-                for (int j = 0; j < n; j++) {
-                    if (s.charAt(j) == '.')
-                        map[i][j] = 0;
-                    else map[i][j] = -1; // 지뢰라면 -1
-                }
-            }
-
-            setMap();
-            gameStart();
-
-            System.out.println("#" + (tc + 1) + " " + click);
+        T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            input(); // 입력
+            setMap(); // 지뢰 개수 세기
+            click(); // 클릭
+            sb.append("#").append(t).append(" ").append(cnt).append("\n");
         }
+
+        System.out.println(sb);
+    }
+
+    static void input() throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        map = new String[N][N];
+        count = new int[N][N];
+        cnt = 0;
+
+        for (int i = 0; i < N; i++)
+            map[i] = br.readLine().split("");
     }
 
     static void setMap() {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (map[i][j] == -1) { // 지뢰라면
-                    for (int t = 0; t < 8; t++) {
-                        int nr = i + dr[t];
-                        int nc = j + dc[t];
-                        if (check(nr, nc) && map[nr][nc] != -1)
-                            map[nr][nc]++;
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+
+                if (map[r][c].equals("*")) { // 현재 칸이 지뢰인 경우
+                    count[r][c] = -1;
+                    for (int i = 0; i < 8; i++) { // 주변 지뢰 아닌 칸들에 +1
+                        int nr = r + dr[i];
+                        int nc = c + dc[i];
+                        if (check(nr, nc) && !map[nr][nc].equals("*"))
+                            count[nr][nc]++;
                     }
                 }
             }
         }
     }
 
-    static void gameStart() { // (0,0)부터 시작
-        click = 0; // 초기화
+    static void click() {
+        boolean visited[][] = new boolean[N][N];
+        Queue<Pos> q = new LinkedList<>();
 
-        // 0 탐색 후 클릭
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (map[i][j] == 0 && !visited[i][j]) { // 값이 0이고 아직 클릭되지 않은 칸
-                    click++;
-                    visited[i][j] = true;
-                    q.add(new Pos(i, j));
+        for (int r = 0; r < N; r++) { // 0의 위치 찾아 큐에 넣기
+            for (int c = 0; c < N; c++) {
 
-                    while (!q.isEmpty()) {
+                if (count[r][c] == 0 && !visited[r][c]) { // 방문하지 않은 0의 경우 클릭 후 방문 처리
+                    cnt++;
+                    visited[r][c] = true;
+                    q.add(new Pos(r, c));
+
+                    while (!q.isEmpty()) { // 주변 탐색
                         Pos p = q.poll();
 
-                        for (int t = 0; t < 8; t++) {
-                            int nr = p.r + dr[t];
-                            int nc = p.c + dc[t];
-                            if (check(nr, nc) && !visited[nr][nc] && map[nr][nc] == 0) {
-                                q.add(new Pos(nr, nc));
+                        for (int i = 0; i < 8; i++) {
+                            int nr = p.r + dr[i];
+                            int nc = p.c + dc[i];
+                            if (check(nr, nc) && !visited[nr][nc] && count[nr][nc] == 0) { // 0인 경우 탐색을 위해 큐에 삽입
                                 visited[nr][nc] = true;
-                            } else if (check(nr, nc) && !visited[nr][nc])
+                                q.add(new Pos(nr, nc));
+                            } else if (check(nr, nc) && !visited[nr][nc]) // 0은 아닌 경우 방문처리만
                                 visited[nr][nc] = true;
                         }
                     }
@@ -90,19 +87,17 @@ public class Solution {
             }
         }
 
-        // 0 아닌거 모두 클릭
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visited[i][j] && map[i][j] != -1) { // 지뢰가 아닌 방문하지 않은 칸
-                    click++;
-                    visited[i][j] = true;
+        for (int r = 0; r < N; r++) { // 0에 인접하지 않아 일일이 클릭해야 하는 경우
+            for (int c = 0; c < N; c++) {
+                if (!visited[r][c] && count[r][c] > 0) {
+                    cnt++;
+                    visited[r][c] = true;
                 }
             }
         }
     }
 
-
     static boolean check(int r, int c) {
-        return 0 <= r && r < n && 0 <= c && c < n;
+        return 0 <= r && r < N && 0 <= c && c < N;
     }
 }
