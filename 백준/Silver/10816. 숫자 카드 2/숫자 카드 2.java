4af7a81595b7d64754ebffd54574aa0d8abff730
ahyolo27 <102876175+ahyolo27@users.noreply.github.com>
2025-03-08 23:51:35 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 796 ms, Memory: 196376 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,69 +1,25 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N, M, cards[], finds[];
-    static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) throws IOException {
-
-        input(); // 입력
-
-        for (int i = 0; i < M; i++)
-            sb.append(upper(cards, finds[i]) - lower(cards, finds[i])).append(" ");
-
-        System.out.println(sb);
-    }
-
-    static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-        N = Integer.parseInt(br.readLine());
-        cards = new int[N];
+        int N = Integer.parseInt(br.readLine());
+        int[] count = new int[20000001];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++)
-            cards[i] = Integer.parseInt(st.nextToken());
-        Arrays.sort(cards); // 오름차순 정렬
+            count[Integer.parseInt(st.nextToken()) + 10000000]++;
 
-        M = Integer.parseInt(br.readLine());
-        finds = new int[M];
+        int M = Integer.parseInt(br.readLine());
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++)
-            finds[i] = Integer.parseInt(st.nextToken());
-    }
+            sb.append(count[Integer.parseInt(st.nextToken()) + 10000000]).append(" ");
 
-    static int upper(int cards[], int n) {
-        int hi = cards.length;
-        int lo = 0;
-        int mid;
-
-        while (lo < hi) {
-            mid = (lo + hi) / 2;
-            if (cards[mid] <= n)
-                lo = mid + 1;
-            else
-                hi = mid;
-        }
-
-        return lo;
-    }
-
-    static int lower(int cards[], int n) {
-        int hi = cards.length;
-        int lo = 0;
-        int mid;
-
-        while (lo < hi) {
-            mid = (lo + hi) / 2;
-            if (cards[mid] < n)
-                lo = mid + 1;
-            else
-                hi = mid;
-        }
-
-        return lo;
+        System.out.println(sb);
     }
 }
