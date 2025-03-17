c7d4acae19b02cb4fdd01ab3882115d92909c267
ahyolo27 <102876175+ahyolo27@users.noreply.github.com>
2025-03-17 14:13:51 +0900

    [Gold IV] Title: 공유기 설치, Time: 280 ms, Memory: 29152 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, C, left, right, home[], ans;
+
+    public static void main(String[] args) throws IOException {
+
+        input(); // 입력
+        set();
+
+        System.out.println(ans);
+    }
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        home = new int[N];
+
+        for (int i = 0; i < N; i++)
+            home[i] = Integer.parseInt(br.readLine());
+
+        Arrays.sort(home);
+        left = 1;
+        right = 1000000001;
+    }
+
+    static void set() {
+        while (left <= right) {
+            int cnt = 1; // 초기화
+            int prev = home[0];
+
+            int mid = (left + right) / 2;
+
+            for (int i = 1; i < home.length; i++) {
+                if (home[i] - prev < mid)
+                    continue;
+                cnt++;
+                prev = home[i];
+            }
+
+            if (cnt >= C) {
+                left = mid + 1;
+                ans = Math.max(mid, ans);
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+}
