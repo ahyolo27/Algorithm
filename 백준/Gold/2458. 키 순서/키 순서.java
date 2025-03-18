010ffabb458579fcc9501249ad115f96f95640f9
ahyolo27 <102876175+ahyolo27@users.noreply.github.com>
2025-03-18 13:47:32 +0900

    [Gold IV] Title: 키 순서, Time: 616 ms, Memory: 42492 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -2,13 +2,15 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static final int INF = 10000000;
-    static int N, M, dist[][], ans;
+    static int N, M, cnt, ans;
+    static boolean visited[];
+    static List<Integer> graph[];
+    static List<Integer> reverseGraph[];
 
     public static void main(String[] args) throws IOException {
 
         input(); // 입력
-        floyd();
+        find();
 
         System.out.println(ans);
     }
@@ -20,38 +22,48 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        dist = new int[N + 1][N + 1];
+        graph = new ArrayList[N + 1];
+        reverseGraph = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                if (i == j) dist[i][j] = 0;
-                else dist[i][j] = INF;
-            }
+            graph[i] = new ArrayList<>();
+            reverseGraph[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            dist[a][b] = 1;
+            graph[a].add(b);
+            reverseGraph[b].add(a); // 역방향 그래프
         }
     }
 
-    static void floyd() {
-        for (int k = 1; k <= N; k++) {
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= N; j++) {
-                    if (dist[i][k] != INF && dist[k][j] != INF)
-                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]); // 갱신
-                }
-            }
-        }
-
+    static void find() {
         for (int i = 1; i <= N; i++) {
-            int cnt = 0;
-            for (int j = 1; j <= N; j++)
-                if (dist[i][j] != INF || dist[j][i] != INF) // 둘 중 하나의 관계만 명확해도 둘 간의 관계를 알 수 있음
-                    cnt++;
-            if (cnt == N) ans++;
+            int sum = 0;
+
+            visited = new boolean[N + 1];
+            cnt = 0;
+            dfs(i, graph);
+            sum += cnt;
+
+            visited = new boolean[N + 1];
+            cnt = 0;
+            dfs(i, reverseGraph);
+            sum += cnt;
+
+            if (sum == N - 1) // 자신 제외 모든 학생과 비교 가능한 경우 count
+                ans++;
+        }
+    }
+
+    static void dfs(int start, List<Integer> g[]) {
+        visited[start] = true;
+        for (int node : g[start]) {
+            if (!visited[node]) {
+                cnt++;
+                dfs(node, g);
+            }
         }
     }
 }
