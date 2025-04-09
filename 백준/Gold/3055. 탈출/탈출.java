d0111a6ef3de7559cd71c5b43d5e5ba77f252c58
ahyolo27 <102876175+ahyolo27@users.noreply.github.com>
2025-04-09 17:48:27 +0900

    [Gold IV] Title: 탈출, Time: 108 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -4,30 +4,26 @@ import java.util.*;
 public class Main {
     static int R, C, ans;
     static char map[][];
-    static boolean visited[][], isArrived;
+    static boolean visited[][];
     static int dr[] = {-1, 0, 1, 0}, dc[] = {0, 1, 0, -1};
     static Queue<Pos> hedgehog = new LinkedList<>();
     static Queue<Pos> water = new LinkedList<>();
 
     static class Pos {
-        int r, c, cnt;
+        int r, c;
 
-        Pos(int r, int c, int cnt) {
+        Pos(int r, int c) {
             this.r = r;
             this.c = c;
-            this.cnt = cnt;
         }
     }
 
     public static void main(String[] args) throws IOException {
 
         input(); // 입력
-        move(); // 이동 시작
+        simulate(); // 시뮬레이션 시작
 
-        if (ans < 0) // ans = -1 이면 도착하지 못하고 갇힌 경우
-            System.out.println("KAKTUS");
-        else
-            System.out.println(ans);
+        System.out.println(ans > 0 ? ans : "KAKTUS");
     }
 
     static void input() throws IOException {
@@ -36,77 +32,70 @@ public class Main {
 
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
+
         map = new char[R][C];
         visited = new boolean[R][C];
-        ans = 0;
+        ans = 0; // 초기화
 
         for (int i = 0; i < R; i++) {
             String str = br.readLine();
             for (int j = 0; j < C; j++) {
                 map[i][j] = str.charAt(j);
-                if (map[i][j] == 'S') {  // 고슴도치 시작점
-                    hedgehog.offer(new Pos(i, j, 0));
+                if (map[i][j] == 'S') {// 시작 위치
+                    hedgehog.offer(new Pos(i, j));
                     map[i][j] = '.';
                     visited[i][j] = true;
-                } else if (map[i][j] == '*') // 물 시작점
-                    water.offer(new Pos(i, j, 0));
+                } else if (map[i][j] == '*') // 물
+                    water.offer(new Pos(i, j));
             }
         }
     }
 
-    static void move() {
-        // 고슴도치 이동
-        while (true) {
-            setWater(); // 홍수 일으키기
+    static void simulate() {
+        int cnt = 0;
 
-            int len = hedgehog.size();
+        while (!hedgehog.isEmpty()) { // 고슴도치가 더 이상 이동할 수 없는 경우 종료
+            // 물 이동
+            int qSize = water.size();
+            for (int i = 0; i < qSize; i++) {
+                Pos w = water.poll();
 
-            while (len > 0) {
-                Pos p = hedgehog.poll();
+                for (int t = 0; t < 4; t++) {
+                    int nr = w.r + dr[t];
+                    int nc = w.c + dc[t];
 
-                for (int i = 0; i < 4; i++) {
-                    int nr = p.r + dr[i];
-                    int nc = p.c + dc[i];
-
-                    if (check(nr, nc) && !visited[nr][nc] && map[nr][nc] == '.') { // 이동할 수 있으면 이동
-                        visited[nr][nc] = true;
-                        hedgehog.offer(new Pos(nr, nc, p.cnt + 1));
-                    } else if (check(nr, nc) && !visited[nr][nc] && map[nr][nc] == 'D') { // 도착하면 종료
-                        ans = p.cnt + 1;
-                        return;
+                    if (isValid(nr, nc) && map[nr][nc] == '.') { // 물 번지기
+                        map[nr][nc] = '*';
+                        water.offer(new Pos(nr, nc));
                     }
                 }
-                if (hedgehog.isEmpty())  // 사방이 막혀 움직일 수 없는 경우
-                    break;
-                len--;
             }
 
-            if (hedgehog.isEmpty()) // 더이상 움직일 수 없는 경우
-                break;
-        }
-        ans = -1;
-    }
+            // 고슴도치 이동
+            qSize = hedgehog.size();
+            for (int i = 0; i < qSize; i++) {
+                Pos h = hedgehog.poll();
 
-    static void setWater() {
-        int len = water.size();
+                if (map[h.r][h.c] == 'D') { // 비버굴에 도착한 경우 종료
+                    ans = cnt;
+                    return;
+                }
 
-        while (len > 0) {
-            Pos p = water.poll();
+                for (int t = 0; t < 4; t++) {
+                    int nr = h.r + dr[t];
+                    int nc = h.c + dc[t];
 
-            for (int i = 0; i < 4; i++) {
-                int nr = p.r + dr[i];
-                int nc = p.c + dc[i];
-
-                if (check(nr, nc) && map[nr][nc] == '.') {
-                    water.offer(new Pos(nr, nc, 0));
-                    map[nr][nc] = '*';
+                    if (isValid(nr, nc) && !visited[nr][nc] && (map[nr][nc] == '.' || map[nr][nc] == 'D')) {
+                        visited[nr][nc] = true;
+                        hedgehog.offer(new Pos(nr, nc));
+                    }
                 }
             }
-            len--;
+            cnt++;
         }
     }
 
-    static boolean check(int r, int c) {
+    static boolean isValid(int r, int c) { // 범위 내 값이면 true
         return 0 <= r && r < R && 0 <= c && c < C;
     }
 }
