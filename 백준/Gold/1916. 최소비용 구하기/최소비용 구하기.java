e563e6d99aff71930d20566e707d5bc67c6da14b
ahyolo27 <102876175+ahyolo27@users.noreply.github.com>
2025-08-26 15:39:29 +0900

    [Gold V] Title: 최소비용 구하기, Time: 460 ms, Memory: 50616 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -3,12 +3,12 @@ import java.io.*;
 
 public class Main {
     static int N, M, start, end, weight[];
-    static List<Node> graph[];
+    static List<Edge> graph[];
 
-    static class Node {
+    static class Edge {
         int node, weight;
 
-        Node(int node, int weight) {
+        Edge(int node, int weight) {
             this.node = node;
             this.weight = weight;
         }
@@ -39,7 +39,7 @@ public class Main {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-            graph[a].add(new Node(b, w)); // 단방향
+            graph[a].add(new Edge(b, w)); // 단방향
         }
 
         st = new StringTokenizer(br.readLine());
@@ -48,20 +48,19 @@ public class Main {
     }
 
     static void dijkstra() {
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o2.weight - o1.weight);
-        pq.add(new Node(start, 0));
+        PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2) -> o1.weight - o2.weight);
+        pq.add(new Edge(start, 0));
         weight[start] = 0;
 
         while (!pq.isEmpty()) {
-            Node n = pq.poll();
+            Edge n = pq.poll();
 
             if (weight[n.node] < n.weight) continue;
 
-            for (int i = 0; i < graph[n.node].size(); i++) {
-                Node next = graph[n.node].get(i);
+            for (Edge next: graph[n.node]) {
                 if (weight[next.node] > weight[n.node] + next.weight) {
                     weight[next.node] = weight[n.node] + next.weight;
-                    pq.add(new Node(next.node, weight[next.node]));
+                    pq.add(new Edge(next.node, weight[next.node]));
                 }
             }
         }
