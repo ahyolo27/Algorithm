25d368c4e8fae8d6fc2e91837ea7001fa6a7dbe6
ahyolo27 <102876175+ahyolo27@users.noreply.github.com>
2025-10-17 10:31:40 +0900

    [Gold III] Title: 나무 재테크, Time: 684 ms, Memory: 167164 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.java
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Main {
 
     static int N, M, K, map[][], foodValue[][];
-    static ArrayList<Integer> trees[][];
+    static Deque<Integer> trees[][];
     static int dr[] = {-1, -1, -1, 0, 1, 1, 1, 0}, dc[] = {-1, 0, 1, 1, 1, 0, -1, -1}; // 8방 탐색
 
     public static void main(String[] args) throws IOException {
@@ -26,11 +26,11 @@ public class Main {
 
         map = new int[N + 1][N + 1]; // 맵 내 양분 정보
         foodValue = new int[N + 1][N + 1]; // 겨울에 얻을 양분 정보
-        trees = new ArrayList[N + 1][N + 1]; // 좌표 별 나무 정보
+        trees = new ArrayDeque[N + 1][N + 1]; // 좌표 별 나무 정보
 
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++)
-                trees[i][j] = new ArrayList<>(); // 양분 정보 초기화
+                trees[i][j] = new ArrayDeque<>(); // 양분 정보 초기화
             Arrays.fill(map[i], 5); // 맵 초기 설정
         }
         for (int i = 1; i <= N; i++) {
@@ -56,32 +56,34 @@ public class Main {
             for (int j = 1; j <= N; j++) {
                 if (trees[i][j].isEmpty()) continue; // 나무가 없다면 지나가기
 
-                int stopIdx = trees[i][j].size();
 
                 // spring
-                trees[i][j].sort(Comparator.comparingInt(o -> o)); // 나무를 오름차순으로 정렬
-                for (int k = 0; k < trees[i][j].size(); k++) {
-                    int tree = trees[i][j].get(k);
-                    if (map[i][j] >= tree) { // 양분이 충분한 경우
+                int size = trees[i][j].size();
+                Deque<Integer> remains = new ArrayDeque<>(size);
+                boolean isFinished = false;
+                int deadSum = 0;
+
+                for (int k = 0; k < size; k++) {
+                    int tree = trees[i][j].pollFirst();
+                    if (!isFinished && map[i][j] >= tree) { // 양분이 충분한 경우
                         map[i][j] -= tree;
-                        trees[i][j].set(k, tree + 1);
+                        remains.addLast(tree + 1);
                     } else {
-                        stopIdx = k;
-                        break;
+                        isFinished = true;
+                        deadSum += tree / 2;
                     }
                 }
+                trees[i][j] = remains; // 살아남은 나무들 다음 계절로 보내기
 
                 // summer
-                for (int k = trees[i][j].size() - 1; k >= stopIdx; k--) {
-                    map[i][j] += trees[i][j].get(k) / 2;
-                    trees[i][j].remove(k);
-                }
+                map[i][j] += deadSum;
             }
         }
 
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                
+                if (trees[i][j].isEmpty()) continue; // 나무가 없다면 지나가기
+
                 // fall
                 for (int tree : trees[i][j]) {
                     if (tree % 5 == 0) {
@@ -89,12 +91,16 @@ public class Main {
                             int nr = i + dr[k];
                             int nc = j + dc[k];
                             if (nr < 1 || nc < 1 || nr > N || nc > N) continue;
-                            trees[nr][nc].add(1);
+                            trees[nr][nc].addFirst(1);
                         }
                     }
                 }
+            }
+        }
 
-                // winter
+        // winter
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
                 map[i][j] += foodValue[i][j];
             }
         }
