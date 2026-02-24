aee9c1a1193bd60b79a79dbc2d29bc31237bb27b
김아현 <102876175+ahyolo27@users.noreply.github.com>
2026-02-24 15:41:50 +0900

    [Gold IV] Title: 전화번호 목록, Time: 560 ms, Memory: 34756 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -2,46 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
-    static class Node {
-        Map<Character, Node> child;
-        boolean isEnd;
-
-        Node() {
-            child = new HashMap<>();
-            isEnd = false;
-        }
-    }
-
-    static class Trie {
-        Node root;
-
-        Trie() {
-            this.root = new Node();
-        }
-
-        boolean insert(String s) {
-            Node node = root;
-            int cnt = 0;
-
-            for (char c : s.toCharArray()) {
-                if (!node.child.containsKey(c)) {
-                    node.child.put(c, new Node());
-                } else {
-                    cnt++;
-                }
-
-                node = node.child.get(c);
-            }
-
-            return cnt != s.length(); // 접두어가 없으면 true, 있으면 false
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -50,17 +12,16 @@ public class Main {
         for (int i = 0; i < T; i++) {
             int N = Integer.parseInt(br.readLine());
 
-            Trie trie = new Trie();
-            boolean finished = false;
-
             String num[] = new String[N];
             for (int j = 0; j < N; j++)
                 num[j] = br.readLine();
 
-            Arrays.sort(num, Comparator.reverseOrder()); // 역순 정렬
+            Arrays.sort(num); // 사전순 정렬
 
-            for (String s : num) {
-                if (!trie.insert(s)) {
+            boolean finished = false;
+
+            for (int j = 0; j < N - 1; j++) {
+                if (num[j + 1].startsWith(num[j])) {
                     finished = true;
                     break;
                 }
