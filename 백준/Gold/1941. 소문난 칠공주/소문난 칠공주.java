470016c9992fea1e1c9a379bfb998c7a9256e33e
김아현 <102876175+ahyolo27@users.noreply.github.com>
2026-03-17 17:30:02 +0900

    [Gold III] Title: 소문난 칠공주, Time: 280 ms, Memory: 67224 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -1,42 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
     static char map[][] = new char[5][5];
     static int dr[] = {-1, 0, 1, 0}, dc[] = {0, 1, 0, -1};
-    static Set<ArrayList<Princess>> answerList;
-
-    static class Princess {
-        int r, c;
-
-        Princess(int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof Princess)) return false;
-
-            Princess princess = (Princess) o;
-            return princess.r == r && princess.c == c;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(r, c);
-        }
-    }
+    static int answer;
 
     public static void main(String[] args) throws IOException {
         input(); // 입력
 
-        calc();
+        comb(0, 0, new int[7]);
 
-        System.out.println(answerList.size());
+        System.out.println(answer);
     }
 
     static void input() throws IOException {
@@ -48,71 +26,63 @@ public class Main {
             for (int j = 0; j < 5; j++)
                 map[i][j] = s.charAt(j);
         }
-
-        answerList = new HashSet<>();
     }
 
-    static void calc() {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                boolean visited[][] = new boolean[5][5];
-                visited[i][j] = true;
-                ArrayList<Princess> princesses = new ArrayList<>();
-                princesses.add(new Princess(i, j));
-
-                if (map[i][j] == 'S')
-                    select(1, 0, princesses, visited);
-                else
-                    select(0, 1, princesses, visited);
-            }
-        }
-
-    }
-
-    static void select(int sCnt, int yCnt, ArrayList<Princess> princesses, boolean visited[][]) {
-        if (sCnt + yCnt == 7) {
-            if (sCnt < yCnt) return;
-            princesses.sort(((o1, o2) -> {
-                if (o1.r == o2.r)
-                    return o1.c - o2.c;
-                return o1.r - o2.r;
-            }));
-            answerList.add(new ArrayList<>(princesses));
+    static void comb(int start, int cnt, int selected[]) {
+        if (cnt == 7) {
+            check(selected);
             return;
         }
 
-        for (Princess p : new ArrayList<>(princesses)) {
+        for (int i = start; i < 25; i++) {
+            selected[cnt] = i;
+            comb(i + 1, cnt + 1, selected);
+        }
+    }
+
+    static void check(int selected[]) {
+        int sCnt = 0;
+        for (int i = 0; i < 7; i++) {
+            int r = selected[i] / 5;
+            int c = selected[i] % 5;
+
+            if (map[r][c] == 'S') sCnt++;
+        }
+
+        if (sCnt < 4) return;
+        if (isConnected(selected)) answer++;
+    }
+
+    static boolean isConnected(int selected[]) {
+        boolean visited[] = new boolean[7];
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(0);
+        visited[0] = true;
+        int cnt = 1;
+
+        while (!q.isEmpty()) {
+            int now = q.poll();
+
+            int r = selected[now] / 5;
+            int c = selected[now] % 5;
+
             for (int i = 0; i < 4; i++) {
-                int nextR = p.r + dr[i];
-                int nextC = p.c + dc[i];
+                int nr = r + dr[i];
+                int nc = c + dc[i];
 
-                if (0 > nextR || 0 > nextC || 5 <= nextR || 5 <= nextC || visited[nextR][nextC]) continue;
+                if (nr < 0 || nc < 0 || nr >= 5 || nc >= 5) continue;
 
-                if (map[nextR][nextC] == 'S') {
-                    visited[nextR][nextC] = true;
-                    sCnt++;
-                    Princess princess = new Princess(nextR, nextC);
-                    princesses.add(princess);
-
-                    select(sCnt, yCnt, princesses, visited);
-
-                    visited[nextR][nextC] = false;
-                    sCnt--;
-                    princesses.remove(princess);
-                    
-                } else if (map[nextR][nextC] == 'Y' && yCnt <= 2) { // 3부터는 +1 시 과반수가 되기 때문에 불가능
-                    visited[nextR][nextC] = true;
-                    yCnt++;
-                    Princess princess = new Princess(nextR, nextC);
-                    princesses.add(princess);
-
-                    select(sCnt, yCnt, princesses, visited);
-
-                    visited[nextR][nextC] = false;
-                    yCnt--;
-                    princesses.remove(princess);
-                }
+                int next = nr * 5 + nc;
+                for (int j = 0; j < 7; j++)
+                    if (!visited[j] && selected[j] == next) {
+                        visited[j] = true;
+                        q.add(j);
+                        cnt++;
+                    }
             }
         }
+
+        return cnt == 7;
     }
 }
