498124a8eebdac3556b55687c4e33e99995379c8
김아현 <102876175+ahyolo27@users.noreply.github.com>
2026-04-21 17:19:06 +0900

    [Silver II] Title: Three Dots, Time: 564 ms, Memory: 29388 KB -BaekjoonHub

--- 백준/Silver/13423. Three Dots/Three Dots.java
+++ 백준/Silver/13423. Three Dots/Three Dots.java
@@ -1,52 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-
-        int T = Integer.parseInt(br.readLine());
-        int cnt;
-        for (int t = 0; t < T; t++) {
-
-            int N = Integer.parseInt(br.readLine());
-
-            st = new StringTokenizer(br.readLine());
-            int arr[] = new int[N];
-            for (int i = 0; i < N; i++)
-                arr[i] = Integer.parseInt(st.nextToken());
-            Arrays.sort(arr); // 오름차순 정렬
-
-            cnt = 0;
-
-            // 투포인터
-            for (int k = 1; k < N - 1; k++) {
-                int left = k - 1;
-                int right = k + 1;
-
-                while (left >= 0 && right < N) {
-                    int sum = arr[left] + arr[right];
-                    int target = 2 * arr[k];
-
-                    if (sum == target) {
-                        cnt++;
-                        left--;
-                        right++;
-                    } else if (sum < target)
-                        right++;
-                    else
-                        left--;
-                }
-            }
-
-            sb.append(cnt).append("\n");
-        }
-
-        System.out.println(sb);
-    }
-}
+<html>
+<head><title>502 Bad Gateway</title></head>
+<body>
+<center><h1>502 Bad Gateway</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
