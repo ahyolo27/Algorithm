28ccf98d02ff1244b61e6ee6e20244bcba08315e
김아현 <102876175+ahyolo27@users.noreply.github.com>
2026-04-23 16:56:29 +0900

    [level 2] Title: 전화번호 목록, Time: 366.09 ms, Memory: 98.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,47 +2,13 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {        
-        Trie trie = new Trie();
+        Arrays.sort(phone_book);
         
-        for(String s: phone_book) {
-            if (!trie.insert(s))
+        for(int i=0;i<phone_book.length-1;i++) {
+            if (phone_book[i+1].startsWith(phone_book[i]))
                 return false;
         }
-
+        
         return true;
-    }
-    
-    class Node {
-        HashMap<Character, Node> child;
-        boolean endOfWord;
-        
-        public Node() {
-            this.child = new HashMap<>();
-            this.endOfWord = false;
-        }
-    }
-    
-    class Trie {
-        Node root;
-        
-        public Trie() {
-            this.root = new Node();
-        }
-        
-        public boolean insert(String s) {
-            Node node = this.root;
-            
-            for(char c: s.toCharArray()) {
-                if (node.endOfWord) return false;
-            
-                node.child.putIfAbsent(c, new Node());
-                node = node.child.get(c);
-            }
-            
-            if (node.endOfWord || !node.child.isEmpty()) return false;
-            
-            node.endOfWord = true;
-            return true;
-        }
     }
 }
