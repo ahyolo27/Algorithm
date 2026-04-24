451f5ecbeb5344d62f8ac114ff3966343ab4ea8e
김아현 <102876175+ahyolo27@users.noreply.github.com>
2026-04-24 16:29:42 +0900

    [level 3] Title: 등굣길, Time: 0.60 ms, Memory: 53.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int dp[][] = new int[n+1][m+1];
+        boolean isPuddle[][] = new boolean[n+1][m+1];
+        
+        dp[1][1] = 1;
+        
+        for(int pud[]: puddles) 
+            isPuddle[pud[1]][pud[0]] = true; // 웅덩이
+        
+        for(int i=1;i<=n;i++) {         
+            for(int j=1;j<=m;j++)  {
+                if (i == 1 && j == 1) continue; // 시작점
+                if (isPuddle[i][j]) continue; // 웅덩이
+                
+                dp[i][j] = (dp[i-1][j]+dp[i][j-1]) % 1_000_000_007;
+            }
+        }
+
+        return dp[n][m];
+    }
+}
