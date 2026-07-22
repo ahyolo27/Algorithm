b4998bb8ca7db8e58aaa91cead70496fe0008ac4
김아현 <102876175+ahyolo27@users.noreply.github.com>
2026-07-22 23:29:30 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 12.75 ms, Memory: 57.5 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -1,31 +1,39 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int alp, int cop, int[][] problems) {
-        int answer = Integer.MAX_VALUE;
-        
-        final int LIMIT = 151;
-
-        int dp[][] = new int[LIMIT][LIMIT]; // dp[알고력][코딩력]=시간
+    public int solution(int alp, int cop, int[][] problems) {       
+        int dp[][] = new int[151][151]; // dp[알고력][코딩력]=시간
         for(int d[]:dp)
             Arrays.fill(d, Integer.MAX_VALUE);
-        dp[alp][cop] = 0; // init
+        
+        int maxAlp = 0;
+        int maxCop = 0;
+        for(int problem[]: problems) {
+            maxAlp = Math.max(maxAlp, problem[0]);
+            maxCop = Math.max(maxCop, problem[1]);
+        }
 
-        // solution        
-        for(int a=alp;a<dp.length;a++) {
-            for(int c=cop;c<dp[a].length;c++) {
-                
+        // solution 
+    
+        if (alp >= maxAlp && cop >= maxCop) return 0; // 이미 짱인 경우
+        
+        alp = Math.min(alp, maxAlp);
+        cop = Math.min(cop, maxCop);
+        dp[alp][cop] = 0; // init
+        
+        for(int a=alp;a<=maxAlp;a++) {
+            for(int c=cop;c<=maxCop;c++) {
+
                 // 도달 불가능한 경우
                 if (dp[a][c] == Integer.MAX_VALUE) continue;
                 
                 // 알고력이나 코딩력을 올리는 경우
-                if (a < LIMIT-1)
+                if (a < maxAlp)
                     dp[a+1][c] = Math.min(dp[a+1][c], dp[a][c]+1);
-                if (c < LIMIT-1)
+                if (c < maxCop)
                     dp[a][c+1] = Math.min(dp[a][c+1], dp[a][c]+1);
 
                 // 문제를 푸는 경우
-                int cnt = 0;
                 for(int problem[]: problems) {
                     int alp_req = problem[0];
                     int cop_req = problem[1];
@@ -35,19 +43,15 @@ class Solution {
                     
                     // 풀 수 있는 경우 -> 풀이
                     if (a >= alp_req && c >= cop_req) {
-                        int aIdx = (a+alp_rwd)>150?150:(a+alp_rwd);
-                        int cIdx = (c+cop_rwd)>150?150:(c+cop_rwd);
+                        int aIdx = (a+alp_rwd)>maxAlp?maxAlp:(a+alp_rwd);
+                        int cIdx = (c+cop_rwd)>maxCop?maxCop:(c+cop_rwd);
                         
                         dp[aIdx][cIdx] = Math.min(dp[aIdx][cIdx], dp[a][c]+cost);
-                        cnt++;
                     }
                 }
-                
-                if (cnt == problems.length) // 문제를 다 풀 수 있는 경우
-                    answer = Math.min(answer, dp[a][c]); // 갱신
             }
         }
         
-        return answer;
+        return dp[maxAlp][maxCop];
     }
 }
